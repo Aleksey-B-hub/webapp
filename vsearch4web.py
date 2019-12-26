bf6f4db28293206c4a02b224aa4a5a33076341ac
Aleksey-B-hub <Aleksey_Bakulin@hotmail.com>
2019-12-26 11:30:38 +0400

    we created class for the connection web server to the database, and retrieving the data from the server.

--- vsearch4web.py
+++ vsearch4web.py
@@ -3,39 +3,26 @@
 from flask import Flask, render_template, request, escape
 from vsearch import search4letters
 
+from DBcm import UseDatabase
+
 app = Flask(__name__)
+app.config['dbconfig'] = dbconfig = {'host': '127.0.0.1',
+		    		     'user': 'vsearch',
+		    		     'password': 'vsearchpasswd',
+		    		     'database': 'vsearchlogDB', }
 
 def log_request(req: 'flask_request', res: str) -> None:
-	
-	dbconfig = {'host': '127.0.0.1',
-		    'user': 'vsearch',
-		    'password': 'vsearchpasswd',
-		    'database': 'vsearchlogDB', }
+    with UseDatabase(app.config['dbconfig']) as cursor:	 
+        _SQL = """insert into log
+		      (phrase, letters, ip, browser_string, results)
+		       values
+		      (%s, %s, %s, %s, %s)"""
 
-	import mysql.connector
-
-	conn = mysql.connector.connect(**dbconfig)
-	cursor = conn.cursor()
-
-	
-	_SQL = """insert into log
-		  (phrase, letters, ip, browser_string, results)
-		  values
-		  (%s, %s, %s, %s, %s)"""
-
-
-	cursor.execute(_SQL, (req.form['phrase'],
+        cursor.execute(_SQL, (req.form['phrase'],
 			      req.form['letters'],
 			      req.remote_addr,
 			      req.user_agent.browser,
 			      res, ))
-
-	conn.commit()
-	cursor.close()
-	conn.close()
-
-
-
 
 
 @app.route('/search4', methods=['Get','Post'])
@@ -62,17 +49,16 @@ def entry_page() ->'html':
 
 @app.route('/viewlog')
 def view_the_log() -> 'html':
-	contents = []
-	with open('vsearch.log') as log:
-		for line in log:
-			contents.append([])
-			for item in line.split('|'):
-				contents[-1].append(escape(item))
-	titles = ('Form Data', 'Remote_addr', 'User_agent', 'Results')
-	return render_template('viewlog.html',
-							the_title='View Log',
-							the_row_titles=titles,
-							the_data=contents,)
+	
+    with UseDatabase(app.config['dbconfig']) as cursor:
+        _SQL = """select phrase, letters, ip, browser_string, results from log"""
+        cursor.execute(_SQL)
+        contents = cursor.fetchall()	
+    titles = ('Phrase', 'Letters', 'Remote_addr', 'User_agent', 'Results')
+    return render_template('viewlog.html',
+			    the_title='View Log',
+			    the_row_titles=titles,
+			    the_data=contents,)
 
 
 
